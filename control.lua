462a7d6a4f49e70ff1f8b7c7873560af6a12c6a7
Hein Wessels <heinwessels93@gmail.com>
2022-07-17 17:03:20 +0200

    Raise build event on undock, and fix colour setting

--- control.lua
+++ control.lua
@@ -259,7 +259,10 @@ function attempt_undock(player, dock_data, force)
         name = serialized_spider.name,
         position = dock.position,
         force = dock.force,
-        create_build_effect_smoke = true
+        create_build_effect_smoke = true,   -- Looks nice
+
+        -- To help other mods keep track of this entity
+        create_entity = true,
     }
     if not spider then
         -- TODO Handle this error nicely!
@@ -323,10 +326,21 @@ script.on_event(defines.events.on_player_used_spider_remote ,
 
 function on_built(event)
     -- If it's a space spidertron, set it to white as default
-    local entity = event.created_entity
+    local entity = event.created_entity or event.entity
     if entity and entity.valid then
         if entity.name == "ss-space-spidertron" then
-            entity.color = {1, 1, 1, 0.5} -- White
+            -- We only want to set it when the user has not set it
+            -- before. However, there's no way we can determine it.
+            -- Usually when it's placed initially the colour is that
+            -- orange-ish colour, and then we turn it white. So we
+            -- assume if it's the orangy-colour then the user has not
+            -- set it, so we turn it to white. 
+            if util.table.compare(
+                entity.color,
+                {r=1, g=0.5, b=0, a=0.5, }
+            ) then
+                entity.color = {1, 1, 1, 0.5} -- White
+            end
         end
     end
 end
