534b6fa77fbf1cb3fb9345b8a6419a056a38c79f
Hein Wessels <heinwessels93@gmail.com>
2022-09-18 14:37:49 +0200

    Space Spidertron now hides in another menu-simulation.

--- prototypes/simulations.lua
+++ prototypes/simulations.lua
@@ -4,55 +4,70 @@ if mods["space-exploration-menu-simulations"] then
     -- Get the simulation I want to tweak
     local menu_simulations = data.raw["utility-constants"]["default"].main_menu_simulations
     local spaceship_dual_shuttles = menu_simulations.spaceship_dual_shuttles
-    if not spaceship_dual_shuttles then return end
+    
+    if spaceship_dual_shuttles then
+        spaceship_dual_shuttles.init = spaceship_dual_shuttles.init..[[
+            local lame_spider = game.surfaces.nauvis.find_entities_filtered{name = "spidertron", limit = 1}[1]
+            if lame_spider then
+                local surface = lame_spider.surface            
+                local position = {-21, -5}
 
-    spaceship_dual_shuttles.init = spaceship_dual_shuttles.init..[[
-        local lame_spider = game.surfaces.nauvis.find_entities_filtered{name = "spidertron", limit = 1}[1]
-        if lame_spider then
-            local surface = lame_spider.surface            
-            local position = {-21, -5}
+                local dock = surface.create_entity{
+                    name="ss-spidertron-dock", position=position}
 
-            local dock = surface.create_entity{
-                name="ss-spidertron-dock", position=position}
-
-            local offset = {0, -0.35}    
-            local render_layer = "object"
-            rendering.draw_sprite{
-                sprite = "ss-docked-spidertron-shadow", 
-                target = dock, 
-                surface = dock.surface,
-                target_offset = offset,
-                render_layer = render_layer,
-            }
-            rendering.draw_sprite{
-                sprite = "ss-docked-spidertron-main", 
-                target = dock, 
-                surface = dock.surface,
-                target_offset = offset,
-                render_layer = render_layer,
-            }
-        
-            rendering.draw_sprite{
-                sprite = "ss-docked-spidertron-tint", 
-                target = dock, 
-                surface = dock.surface,
-                tint = {a=0.5, b=0, g=1, r=0},
-                target_offset = offset,
-                render_layer = render_layer,
-            }
-            rendering.draw_animation{
-                animation = "ss-docked-light", 
-                target = dock, 
-                surface = dock.surface,
-                target_offset = offset,
-                animation_offset = math.random(15),
-                render_layer = render_layer,
-            }
+                local offset = {0, -0.35}    
+                local render_layer = "object"
+                rendering.draw_sprite{
+                    sprite = "ss-docked-spidertron-shadow", 
+                    target = dock, 
+                    surface = dock.surface,
+                    target_offset = offset,
+                    render_layer = render_layer,
+                }
+                rendering.draw_sprite{
+                    sprite = "ss-docked-spidertron-main", 
+                    target = dock, 
+                    surface = dock.surface,
+                    target_offset = offset,
+                    render_layer = render_layer,
+                }
             
-            lame_spider.destroy()
-        end
-    ]]
+                rendering.draw_sprite{
+                    sprite = "ss-docked-spidertron-tint", 
+                    target = dock, 
+                    surface = dock.surface,
+                    tint = {a=0.5, b=0, g=1, r=0},
+                    target_offset = offset,
+                    render_layer = render_layer,
+                }
+                rendering.draw_animation{
+                    animation = "ss-docked-light", 
+                    target = dock, 
+                    surface = dock.surface,
+                    target_offset = offset,
+                    animation_offset = math.random(15),
+                    render_layer = render_layer,
+                }
+                
+                lame_spider.destroy()
+            end
+        ]]
+        
+        -- For debugging
+        -- data.raw["utility-constants"]["default"].main_menu_simulations = {spaceship_dual_shuttles}
+    end
 
-    -- For debugging
-    -- data.raw["utility-constants"]["default"].main_menu_simulations = {spaceship_dual_shuttles}
+    
+    local space_mat_impact_data_scrap = menu_simulations.space_mat_impact_data_scrap
+    if space_mat_impact_data_scrap then
+        space_mat_impact_data_scrap.init = space_mat_impact_data_scrap.init..[[
+            local space_spidy = game.surfaces.nauvis.create_entity{
+                name="ss-space-spidertron",
+                position={-21, 4}
+            }
+            space_spidy.color = {1, 1, 1, 0.5}
+        ]]
+        -- For debugging
+        -- data.raw["utility-constants"]["default"].main_menu_simulations = {space_mat_impact_data_scrap}
+    end
 end
