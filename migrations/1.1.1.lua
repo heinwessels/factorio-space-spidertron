a81a2785e7490855299d7cb208d177e93d322ebc
Hein Wessels <heinwessels93@gmail.com>
2022-10-01 20:56:25 +0200

    Fix specific dock crash after migration issue

---
+++ migrations/1.1.1.lua
@@ -0,0 +1,29 @@
+-- Theres a bug in either 1.0.0 or 1.1.0 migration scripts to results
+-- in the dock_data internal unit number becoming out of sync with
+-- what it should be. Not sure where this happens, but it doesn't
+-- really matter, because anyone that migrated the same way will
+-- have the same issue already. Better write a migration that 
+-- simply fixes it
+-- Reported by Alphaprime
+
+if not global.docks then return end
+
+local marked_for_fixing = {}
+
+-- Find all broken dock data's
+for unit_number, dock_data in pairs(global.docks) do
+    local dock = dock_data.dock_entity
+    if dock.unit_number ~= unit_number or dock.unit_number ~= dock_data.unit_number then
+        table.insert(marked_for_fixing, unit_number)
+    end
+end
+
+-- Fix them by using the entity's unit number
+-- as the source of truth
+for _, unit_number in pairs(marked_for_fixing) do
+    local dock_data = global.docks[unit_number]
+    local dock = dock_data.dock_entity
+    dock_data.unit_number = dock.unit_number
+    global.docks[dock.unit_number] = dock_data
+    global.docks[unit_number] = nil
+end
